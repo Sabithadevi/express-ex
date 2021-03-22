456697648711ae7187df41565e33b55984ac1cc2
Sabithadevi <80668104+Sabithadevi@users.noreply.github.com>
2021-03-22 12:10:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 //Home page route
-app.get('/', (req, res)=> res.render('index', {title: 'AWS - Add Member Application1', members}));
+app.get('/', (req, res)=> res.render('index', {title: 'AWS - Add Member Application portal', members}));
 
 
 //Member API routes
